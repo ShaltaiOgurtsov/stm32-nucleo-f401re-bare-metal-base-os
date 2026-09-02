9bd3143f4f6064b7fe0ee798c920a123078095b6
ShaltaiOgurtsov <75531600+ShaltaiOgurtsov@users.noreply.github.com>
2026-09-02 21:05:14 +0300

    Added console header

---
+++ Core/Inc/console.h
@@ -0,0 +1,18 @@
+#ifndef  _CONSOLE_H_
+#define _CONSOLE_H_
+
+#include <stdint.h>
+
+#include "ttys.h"
+
+stuct console_cfg{
+
+};
+
+//Core interface module functions
+
+int32_t console_get_def_cfg(struct console_cfg* cfg);
+int32_t console_init(struct console_cfg* cfg);
+int32_t console_run(void);
+
+#endif //_CONSOLE_H_
